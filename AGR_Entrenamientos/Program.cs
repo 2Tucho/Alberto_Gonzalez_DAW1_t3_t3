4ca76268a75577a7bd3b0cc86cf528e3ad96d29b
2Tucho <albergromero@gmail.com>
2026-02-20 14:22:45 +0100

    Añadida la lógica para la opción Login

--- AGR_Entrenamientos/Program.cs
+++ AGR_Entrenamientos/Program.cs
@@ -1,18 +1,46 @@
 ﻿Dictionary<string, string> usuarios = new Dictionary<string, string>();
+usuarios.Add("agr@gmail.com", "1234");
 int opcion;
 do
 {
-    Console.WriteLine("SISTEMA DE GESTIÓN DE ENTRENAMIENTOS\n  1. Login\n  2. Registro\n  3. Cerrar aplicación");
+    Console.WriteLine("SISTEMA DE GESTIÓN DE ENTRENAMIENTOS\n  1. Login\n  2. Registro\n  3. Cerrar aplicación\n");
     Console.WriteLine("Indica la opción deseada escribiendo su número de opción (1-3)");
 
     opcion = int.Parse(Console.ReadLine());
-    usuarios.Add("agr@gmail.com", "1234");
 
     switch (opcion)
     {
         case 1:
-            Console.WriteLine($"La opcion elegida es {opcion}");
+            Console.WriteLine("\n---LOGIN---");
+            string correoLogin, passwordLogin;
+            do
+            {
+                Console.WriteLine("Dime un correo electrónico:");
+                correoLogin = Console.ReadLine();
+                if (!usuarios.ContainsKey(correoLogin))
+                {
+                    Console.WriteLine("Ese correo no está registrado, prueba otro diferente.\n");
+                    opcion = 4;
+                    break;
+                }
+            } while (!usuarios.ContainsKey(correoLogin));
+
+            do
+            {
+                Console.WriteLine("\nDime una contraseña:");
+                passwordLogin = Console.ReadLine();
+
+                if (usuarios[correoLogin] == passwordLogin)
+                {
+                    Console.WriteLine("CONTRASEÑA OK");
+                } else if (usuarios[correoLogin] != passwordLogin)
+                {
+                    Console.WriteLine("\nEsa contraseña no es válida, prueba otra diferente.");
+                }
+            } while (usuarios[correoLogin] != passwordLogin);
+
             break;
+
         case 2:
             Console.WriteLine("\n---REGISTRO---");
             string correoRegistro, passwordRegistro;
@@ -22,7 +50,7 @@ do
                 correoRegistro = Console.ReadLine();
                 if (usuarios.ContainsKey(correoRegistro))
                 {
-                    Console.WriteLine("Es correo ya existe, prueba otro diferente.\n");
+                    Console.WriteLine("Ese correo ya existe, prueba otro diferente.\n");
                 }
             } while (usuarios.ContainsKey(correoRegistro));
 
@@ -37,48 +65,13 @@ do
 
             opcion = 4;
             break;
+
         case 3:
             break;
+
         default:
             Console.WriteLine("\nOpción no válida. Introduce un número del 1 al 3 dependiendo de la opción que quieras escoger.\n");
             break;
     }
-
-    // {
-    //     string correoRegistro, passwordRegistro;
-    //     do
-    //     {
-    //         Console.WriteLine("Dime correo para el registro:");
-    //         correoRegistro = Console.ReadLine();
-    //         //Console.WriteLine("Es correo ya existe, prueba otro diferente.");
-    //     } while (usuarios.ContainsKey(correoRegistro));
-
-    //     Console.WriteLine("Y dime una contraseña:");
-    //     passwordRegistro = Console.ReadLine();
-    //     usuarios.Add(correoRegistro, passwordRegistro);
-
-    //     foreach (KeyValuePair<string, string> usuario in usuarios)
-    //         {
-    //             Console.WriteLine($"Email: {usuario.Key} - Contraseña: {usuario.Value}");
-    //         }
-
-    // Console.WriteLine("Dime correo para el registro:");
-    // string correoRegistro = Console.ReadLine();
-
-    // if (usuarios.ContainsKey(correoRegistro))
-    // {
-    //     Console.WriteLine($"\nUsuario {correoRegistro} ya existe en la base de datos");
-    //     opcion = 2;
-    // }
-    // else
-    // {
-    //     Console.WriteLine("Y dime una contraseña:");
-    //     string passwordRegistro = Console.ReadLine();
-    //     usuarios.Add(correoRegistro, passwordRegistro);
-    //     foreach (KeyValuePair<string, string> usuario in usuarios)
-    //     {
-    //         Console.WriteLine($"Email: {usuario.Key} - Contraseña: {usuario.Value}");
-    //     }
-    // }
 
 } while (opcion <= 0 || opcion >= 4);
