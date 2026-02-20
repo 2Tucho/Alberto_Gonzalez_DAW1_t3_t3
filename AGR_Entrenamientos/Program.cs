3e00b72297cb51fbedcf7721741b43a54ceb3eff
2Tucho <albergromero@gmail.com>
2026-02-20 16:03:39 +0100

    Arreglado fallo en el Login Me sobraban un par de líneas que me hacían salirme del bucle de la comprobación del correo

--- AGR_Entrenamientos/Program.cs
+++ AGR_Entrenamientos/Program.cs
@@ -1,6 +1,7 @@
 ﻿Dictionary<string, string> usuarios = new Dictionary<string, string>();
 usuarios.Add("agr@gmail.com", "1234");
 int opcion;
+
 do
 {
     Console.WriteLine("SISTEMA DE GESTIÓN DE ENTRENAMIENTOS\n  1. Login\n  2. Registro\n  3. Cerrar aplicación\n");
@@ -20,8 +21,6 @@ do
                 if (!usuarios.ContainsKey(correoLogin))
                 {
                     Console.WriteLine("Ese correo no está registrado, prueba otro diferente.\n");
-                    opcion = 4;
-                    break;
                 }
             } while (!usuarios.ContainsKey(correoLogin));
 
@@ -32,21 +31,50 @@ do
 
                 if (usuarios[correoLogin] == passwordLogin)
                 {
-                    Console.WriteLine($"¡Bienvenido al sistema de entrenamientos {correoLogin}!\n  1. Registar entrenamiento\n  2. Lista de entrenamientos\n  3. Vaciar lista de entrenamientos\n  4. Cerrar sesión\n");
+                    int opcionEntreno;
+                    do
+                    {
 
+                        Console.WriteLine($"¡Bienvenido al sistema de entrenamientos {correoLogin}!\n  1. Registar entrenamiento\n  2. Lista de entrenamientos\n  3. Vaciar lista de entrenamientos\n  4. Cerrar sesión\n");
 
+                        Console.WriteLine("Indica la opción deseada escribiendo su número de opción (1-4)");
+                        opcionEntreno = int.Parse(Console.ReadLine());
+
+                        switch (opcionEntreno)
+                        {
+                            case 1:
+                            Console.WriteLine("\n---REGISTRO ENTRENO---");
+                                break;
+                            case 2:
+                            Console.WriteLine("\n---LISTA DE ENTRENOS---");
+                                break;
+                            case 3:
+                            Console.WriteLine("\nLista de entrenamientos vaciada con éxito");
+                                break;
+                            case 4:
+                                break;
+                            default:
+                                Console.WriteLine("\nOpción no válida. Introduce un número del 1 al 4 dependiendo de la opción que quieras escoger.\n");
+                                break;
+                        }
+                    //} while (opcionEntreno <= 0 || opcionEntreno >= 4);
+                    } while (opcionEntreno != 4);
+
+                    break;
                 }
-                else if (usuarios[correoLogin] != passwordLogin)
+                else
                 {
                     Console.WriteLine("\nEsa contraseña no es válida, prueba otra diferente.");
                 }
-            } while (usuarios[correoLogin] != passwordLogin);
+            //} while (usuarios[correoLogin] != passwordLogin);
+            } while (true);
 
             break;
 
         case 2:
             Console.WriteLine("\n---REGISTRO---");
             string correoRegistro, passwordRegistro;
+
             do
             {
                 Console.WriteLine("Dime un correo electrónico:");
@@ -77,4 +105,4 @@ do
             break;
     }
 
-} while (opcion <= 0 || opcion >= 4);
+} while (opcion != 3);
